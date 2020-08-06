f2da11f92f80d9083dc3c03d2671b7307e2f7b41
Ben <ben.lemaitre@protonmail.com>
2020-08-07 01:50:51 +0200

    Updates

---
+++ precache-manifest.984bc18631899cc33151e7fee4d53c3f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aa0291144f7b8ebdb3fd968f8916c4a",
+    "url": "/users-cards/index.html"
+  },
+  {
+    "revision": "5768e4787759155aba19",
+    "url": "/users-cards/static/css/main.a947f6ed.chunk.css"
+  },
+  {
+    "revision": "eda02dc4d12c2fa2e84a",
+    "url": "/users-cards/static/js/2.4af8ce82.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/users-cards/static/js/2.4af8ce82.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5768e4787759155aba19",
+    "url": "/users-cards/static/js/main.a151f6b1.chunk.js"
+  },
+  {
+    "revision": "cc41bff62e6560a212de",
+    "url": "/users-cards/static/js/runtime-main.d4a15cb1.js"
+  }
+]);
